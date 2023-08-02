6f778a4d2e7f13de8d6f3faa4635e542c11e8255
GibranKSR <soto.gibran.isw@unipolidgo.edu.mx>
2023-08-02 14:46:21 -0600

    1

--- models/userORM.js
+++ models/userORM.js
@@ -1,34 +1,34 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize({
-    host: "localhost",
-    username: "root",
-    password: "1234",
-    database: "motion",
-    dialect: "mysql",
-    define: {
-        timestamps: false
-    }
+  host: "b6u2n4lxomwtivnohur7-mysql.services.clever-cloud.com",
+  username: "u2vmfqukfp1vbi5j",
+  password: "hCuIIlkokzvsHpMSQ8pJ",
+  database: "b6u2n4lxomwtivnohur7",
+  dialect: "mysql",
+  define: {
+    timestamps: false,
+  },
 });
 
 const UserORM = sequelize.define("users", {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    name: {
-        type: DataTypes.TEXT
-    },
-    email: {
-        type: DataTypes.TEXT
-    },
-    password: {
-        type: DataTypes.TEXT
-    },
-    avatar: {
-        type: DataTypes.TEXT
-    }
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.TEXT,
+  },
+  email: {
+    type: DataTypes.TEXT,
+  },
+  password: {
+    type: DataTypes.TEXT,
+  },
+  avatar: {
+    type: DataTypes.TEXT,
+  },
 });
 
 module.exports = UserORM;
